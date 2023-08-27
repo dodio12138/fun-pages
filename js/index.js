4f9f134c087d9f1d01ee725f0d9969303fe50dab
dodio12138 <1159715241@qq.com>
2023-08-27 01:45:34 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ var close = document.getElementById("close");
 var windows = document.getElementById("mainWindow")
 var windowBody = document.getElementById("windowbody")
 var statusBody = document.getElementById("statusbody")
+var isClone = false;
 // 添加点击事件处理程序
 minimize.addEventListener("click", function() {
     // 在这里执行你的命令
@@ -51,3 +52,34 @@ document.addEventListener("mouseup", function () {
     isDragging = false;
     //windows.style.cursor = "grab";
   });
+
+function checkClose(){
+    if(windows.style.display === 'none' && isClone == false ){        for(let i = 0; i < 10; i++){
+            setTimeout(() => {
+                const divCopy = windows.cloneNode(true);
+                // Calculate random x and y coordinates
+                const x = Math.floor(Math.random() * (0.9-0.2) * window.innerWidth);
+                const y = Math.floor(Math.random() * (0.9-0.2) * window.innerHeight);            
+                // Set the position of the div copy
+                divCopy.style.position = 'absolute';
+                divCopy.style.left = x + 'px';
+                divCopy.style.top = y + 'px';
+                divCopy.style.display = 'block';
+                //divCopy.childNodes[0].classList.add('inactive');
+                divCopy.childNodes[1].childNodes[3].remove();
+                divCopy.childNodes[1].className = 'title-bar inactive';
+                //divCopy.classList.add('title-bar', 'inactive');
+                //console.log(divCopy.childNodes);
+                //console.log(divCopy.childNodes[1]);
+                // Append the div copy to the document body
+                document.body.appendChild(divCopy);
+              }, i * 100);
+        }
+        isClone = true;
+    }
+}
+
+setInterval(() => {
+    // Perform detection
+    checkClose();
+  }, 100);
